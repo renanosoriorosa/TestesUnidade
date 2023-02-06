0a2c3f1cdb3c690a4e1ba5d5391d0be3742eef07
Renan Osorio <renanosoriogd@gmail.com>
2023-02-06 20:55:57 -0300

    Iniciado testes de integração com .net 2.2

---
+++ 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoItemTests.cs
@@ -0,0 +1,20 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class PedidoItemTests
+    {
+        [Fact(DisplayName = "Novo Item Pedido Com Unidades Abaixo do Permitido")]
+        [Trait("Categoria", "Vendas - Pedido Item")]
+        public void AdicionarItemPedido_UnidadesItemAbaixoDoPermitido_DeveRetornarException()
+        {
+            //Arrange, Act e Assert
+            Assert.Throws<DomainException>(() => new PedidoItem(Guid.NewGuid(), "Produto Teste", Pedido.MIN_UNIDADES_ITEM - 1, 100));
+        }
+    }
+}
